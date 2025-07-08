d0a6abcf141e4000b439dfb118e5dafd95fa1e3c
DarkMechanikum <v.shtarev08@gmail.com>
2025-07-08 17:21:00 +0300

    added the first version of sources and cmake

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include "uart_bitbang_sender.hpp"
+#include <chrono>
+#include <thread>
+
+int main() {
+    BitBangUartSender sender("gpiochip1", 259, 9600); // GPIO17 (BCM), 9600 baud
+    if (!sender.start()) return 1;
+
+    double x = 0.0;
+    while (true) {
+        sender.set_value(x);
+        x += 0.25;
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+}
---
+++ src/uart_bitbang_sender.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include <atomic>
+#include <chrono>
+#include <future>
+#include <gpiod.h>
+#include <string>
+#include <thread>
+
+class BitBangUartSender {
+public:
+    BitBangUartSender(const std::string& chipname, unsigned int tx_pin, unsigned int baudrate = 9600);
+    ~BitBangUartSender();
+
+    void set_value(double value);
+    bool start();
+    void stop();
+
+private:
+    std::string chipname;
+    unsigned int tx_pin;
+    int delay_us;
+
+    struct gpiod_chip* chip = nullptr;
+    struct gpiod_line* tx_line = nullptr;
+
+    std::atomic<double> value_to_send;
+    std::atomic<bool> running;
+    std::thread thread;
+
+    void tx_loop();
+    void send_byte(uint8_t byte);
+    void send_start_bit();
+    void send_stop_bit();
+    void send_char(char c);
+    void send_string(const std::string& str);
+};
